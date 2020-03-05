0a8131afda77a9d31a81c5945f2650a91c89aabb
Dawid Mucha <dawidmucha98@gmail.com>
2020-03-05 23:30:42 +0100

    table with companies

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import update from 'immutability-helper'
+import _ from 'lodash'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      companies: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://recruitment.hal.skygate.io/companies').then((res, rej) => { // get the list of all companies
+      return res.json()
+    }).then(listOfCompanies => {
+      listOfCompanies.map(company => { // map through all companies
+        fetch(`https://recruitment.hal.skygate.io/incomes/${company.id}`).then((res, rej) => { // fetch the incomes of a specific company
+          return res.json()
+        }).then(companyIncomes => {
+          const listOfCompaniesMatchedCompany = listOfCompanies.filter(company => { // filter list of companies...
+            return company.id === companyIncomes.id // ...to match only when its id equls the id of incomes from a specific company
+          })[0] // turn 1-element array into object
+
+          this.setState(state => ({
+            companies: [...state.companies, {...listOfCompaniesMatchedCompany, ...companyIncomes}] // spread both pieces to create one joint object
+          }))
+        })
+      })
+    }).catch(err => console.log(err))
+  }
+
+  render() {
+    const companies = this.state.companies.map(company => {
+      return (
+        <tr key={company.id}>
+          <td>{company.id}</td>
+          <td>{company.name}</td>
+          <td>{company.city}</td>
+          <td>TBD</td>
+          <td>TBD</td>
+          <td>TBD</td>
+        </tr>
+      )
+    })
+
+    return (
+      <div>
+        <table style={{border: '1px solid black'}}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>City</th>
+              <th>Name</th>
+              <th>total income</th>
+              <th>avg income</th>
+              <th>last month income</th>
+            </tr>
+          </thead>
+          <tbody>
+            {companies}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
 export default App;
