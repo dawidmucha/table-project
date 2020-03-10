62167f9b2557afa0fd9eed0368ac28a4ad6b268e
Dawid Mucha <dawidmucha98@gmail.com>
2020-03-10 05:41:56 +0100

    build forlder created

---
+++ build/precache-manifest.712a2ce63f47892aa9cf89941da0fac2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97c3086fa5d30299dbc97c98e6557858",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a93c939c2bd677ebdd9",
+    "url": "/static/css/main.e117c89a.chunk.css"
+  },
+  {
+    "revision": "164b8fcd3271a8041c9e",
+    "url": "/static/js/2.d84498bd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.d84498bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a93c939c2bd677ebdd9",
+    "url": "/static/js/main.101ba067.chunk.js"
+  },
+  {
+    "revision": "0f7a5034e02593c71f4e",
+    "url": "/static/js/runtime-main.9e472acf.js"
+  }
+]);
