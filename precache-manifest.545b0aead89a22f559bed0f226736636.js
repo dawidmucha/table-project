06018b44eb992b4850f1f948575f93d2aa5506f9
Dawid Mucha <dawidmucha98@gmail.com>
2020-03-10 06:07:49 +0100

    Updates

---
+++ precache-manifest.545b0aead89a22f559bed0f226736636.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d551a921968b907db60d291e92c442d8",
+    "url": "/table-project/index.html"
+  },
+  {
+    "revision": "2a93c939c2bd677ebdd9",
+    "url": "/table-project/static/css/main.e117c89a.chunk.css"
+  },
+  {
+    "revision": "164b8fcd3271a8041c9e",
+    "url": "/table-project/static/js/2.d84498bd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/table-project/static/js/2.d84498bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a93c939c2bd677ebdd9",
+    "url": "/table-project/static/js/main.101ba067.chunk.js"
+  },
+  {
+    "revision": "4d7db09c97abf640f45f",
+    "url": "/table-project/static/js/runtime-main.43006b6d.js"
+  }
+]);
